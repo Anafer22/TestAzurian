db34a3f50dd68ab14f7746a9ff674719eb6c88a8
AnaFer <feraral31@gmail.com>
2023-07-02 16:35:01 -0600

    Inicializacion prueba

---
+++ src/main/java/com/azurian/test/TestAzurianApplication.java
@@ -0,0 +1,13 @@
+package com.azurian.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAzurianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAzurianApplication.class, args);
+	}
+
+}
